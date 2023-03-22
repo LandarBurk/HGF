5acff02ac1ac7ebc7fb811fa88cde5ee6bec0622
LandarBurk <128561951+LandarBurk@users.noreply.github.com>
2023-03-22 04:35:58 +0200

    Update fam.cpp

--- fam.cpp
+++ fam.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
 class Family {
@@ -8,22 +9,23 @@ public:
 };
 
 int main() {
-    const int family_size = 4;
-    Family family[family_size];
+    std::vector<Family> family;
 
     std::cout << "Please enter the names and ages of each family member:" << std::endl;
-    for (int i = 0; i < family_size; i++) {
+    for (int i = 0; i < 4; i++) {
         std::cout << "Family member #" << i + 1 << ":" << std::endl;
+        Family member;
         std::cout << "Name: ";
-        std::getline(std::cin, family[i].name);
+        std::getline(std::cin, member.name);
         std::cout << "Age: ";
-        std::cin >> family[i].age;
+        std::cin >> member.age;
         std::cin.ignore();
+        family.push_back(member);
     }
 
     std::cout << std::endl << "The happy German family members are:" << std::endl;
-    for (int i = 0; i < family_size; i++) {
-        std::cout << "Name: " << family[i].name << ", Age: " << family[i].age << std::endl;
+    for (const auto& member : family) {
+        std::cout << "Name: " << member.name << ", Age: " << member.age << std::endl;
     }
 
     return 0;
